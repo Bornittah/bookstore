237bc5188112a06df7bac6c71cb89dce72c03c47
bornittah <bornittah@gmail.com>
2022-12-15 18:29:47 +0300

    Connect to api

--- src/redux/books/books.jsx
+++ src/redux/books/books.jsx
@@ -1,52 +1,40 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { postBook, getBooks, deleteBook } from '../../api/bookAPI';
+
 const ADD_BOOK = 'book-store/books/ADD_BOOK';
+const FETCH_BOOKS = 'book-store/books/FETCH_BOOKS';
 const REMOVE_BOOK = 'book-store/books/REMOVE_BOOK';
-// Reducer
-let books = [];
-export default function booksReducer(state = books, action) {
-  books = [{
-    id: 1,
-    title: 'Learn how to code',
-    author: 'Bornittah',
+
+// fetch books from the api
+export const fetchBooks = createAsyncThunk(FETCH_BOOKS,
+  async () => {
+    const response = await getBooks();
+    return response;
+  });
+// add book to api
+export const addBook = createAsyncThunk(ADD_BOOK,
+  async (bookdetails) => {
+    const response = await postBook(bookdetails);
+    await fetchBooks();
+    return response;
+  });
+
+// remove book from the api
+export const removeBook = createAsyncThunk(REMOVE_BOOK,
+  async (bookId) => {
+    const response = await deleteBook(bookId);
+    await fetchBooks();
+    return response;
+  });
+
+const slice = createSlice({
+  name: 'books',
+  initialState: [],
+  extraReducers: {
+    [addBook.fulfilled]: (state, action) => action.payload,
+    [fetchBooks.fulfilled]: (state, action) => action.payload,
+    [removeBook.fulfilled]: (state, action) => action.payload,
   },
-  {
-    id: 2,
-    title: 'React-Redux Course',
-    author: 'Bornittah',
-  }];
-  switch (action.type) {
-    case ADD_BOOK:
-      return [
-        ...state,
-        {
-          id: action.id,
-          title: action.title,
-          author: action.author,
-        },
-      ];
-
-    case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.id);
-
-    default:
-      return state;
-  }
-}
-
-// actions
-export const addBook = (bookTitle, bookAuthor, bookId) => {
-  const book = {
-    type: ADD_BOOK,
-    title: bookTitle,
-    author: bookAuthor,
-    id: bookId,
-  };
-  return book;
-};
-
-export const removeBook = (bookId) => {
-  const removedBook = {
-    type: REMOVE_BOOK,
-    id: bookId,
-  };
-  return removedBook;
-};
+});
+export const booksList = (state) => state.bookList;
+export default slice.reducer;
